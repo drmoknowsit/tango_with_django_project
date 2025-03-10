7eca979abc0bdc0edde51a6925d170d5f116ab29
“Maureen <0509152M@student.gla.ac.uk>
2025-03-10 20:13:54 +0000

    Ch7 before exercises

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from rango.forms import CategoryForm
 from rango.models import Category
 from rango.models import Page
 
@@ -28,4 +30,14 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context=context_dict)
 
+def add_category(request):
+    form = CategoryForm()
 
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})

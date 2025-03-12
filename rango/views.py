59bb041337c42c8cdab0fad0cec5ff6f799a145a
“Maureen <0509152M@student.gla.ac.uk>
2025-03-12 21:35:08 +0000

    Ch10 done before exercises

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from urllib import response
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
@@ -6,19 +9,43 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from rango.models import Category, Page
 from django.http import HttpResponse
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).seconds > 0:
+        visits += 1
+        request.session['last_visit']= str(datetime.now())
+    else:
+        request.session['last_visit']=  last_visit_cookie
+    request.session['visits']= visits
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
+
     context_dict = {}
     context_dict['boldmessage']= 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    return render(request, 'rango/index.html', context=context_dict )
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/index.html', context=context_dict )
+
+    return response
 
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Dr Mo.'}
+
     return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):

fceb066571a54acf8e21dce73451d10460d7eba6
“Maureen <0509152M@student.gla.ac.uk>
2025-03-12 20:37:44 +0000

    Ch9 completed all tests passed.

--- rango/views.py
+++ rango/views.py
@@ -33,6 +33,7 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -45,6 +46,7 @@ def add_category(request):
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -128,7 +130,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 
 @login_required
